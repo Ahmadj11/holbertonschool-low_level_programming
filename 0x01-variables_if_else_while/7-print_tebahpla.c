9133666b32144aec889419df7d4f066542e2dab9
root <Ahmadj.file@gmail.com>
2022-01-25 16:07:28 -0800

    print teb try1

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - printing lowercase letters
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	char x;
+for (x = 'z'; x <= 'a'; --x)
+putchar(x);
+putchar('\n');
+return (0);
