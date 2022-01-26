d1e62505f06bb16077ea26d7adbdee0857edec00
root <Ahmadj.file@gmail.com>
2022-01-26 12:37:27 -0800

    just try4

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * main - print alphabet
  * Return: 0
  */
 int _putchar(char x)
 {
-	for (x = 'a'; x <= 'z'; x, x++)
-		_putchar('x');
-		_putchar('\n');
-return (0);
+for (x = 'a'; x <= 'z'; x, x++)
+_putchar('x');
+_putchar('\n');
 }
