3eff2e871bef2c7cbe7b4428f8523b1f75742da4
root <Ahmadj.file@gmail.com>
2022-01-26 11:27:48 -0800

    alphabet try1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - print alphabet
+ * Return: 0
+ */
+void print_alphabet(void);
+{
+	for(x = 'a'; x <= 'z'; x, x++)
+		_putchar('x');
+		_putchar('\n');
+return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,4 +10,5 @@
 
 /* _putchar prototype */
 int _putchar(char c);
+void print_alphabet(void);
 #endif /* MAIN_H */
