1f9fb033e78bc4fa88f975d3de1c6bc51e33f89c
root <Ahmadj.file@gmail.com>
2022-02-02 14:47:41 -0800

    reset try3

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * main - resets to 98 
- *
+ * reset_to_98 - output 98
+ * @n: variable 1
  * Return: Always 0
  */
 void reset_to_98(int *n)
-{
+		{
 	*n = 98;
-}
+	}
 
 
 
