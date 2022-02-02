98d9fef6ca0d523105ba5af1b5a623bfa6f7cfd0
root <Ahmadj.file@gmail.com>
2022-02-02 12:12:53 -0800

    save state 2/2

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - resets to 98 
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n);
+{
+	int n = 0;
+	int *n = &n;
+	prrint 
+
+
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is the README.md for pointers strings and arrays 
