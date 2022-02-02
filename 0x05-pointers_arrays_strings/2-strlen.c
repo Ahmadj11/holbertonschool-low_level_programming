2e46b372bc4b10fab23261e9d11a3cb455850865
root <Ahmadj.file@gmail.com>
2022-02-02 15:20:02 -0800

    string try1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - the length of string
+ * @s: firsr var
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int v;
+
+	for (v = 0; s[v] != '\0'; v++)
+	{
+		continue;
+	}
+	return (v);
+}
