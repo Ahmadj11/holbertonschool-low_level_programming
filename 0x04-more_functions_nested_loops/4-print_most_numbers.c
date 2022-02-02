df53a9b150854c0fee824835f573d236a318a01e
root <Ahmadj.file@gmail.com>
2022-02-01 20:16:09 -0800

    print most num try1

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include"main.c"
+#include <stdio.h>
+/**
+ * print_most_numbers - prints 0-9
+ *
+ * Return (0)
+ */
+void print_most_numbers(void);
+{
+	int count:
+		for (count = '0'; count <= '9'; count++)
+		{
+			if (count != '4' && count != '2')
+			{
+				_putchar(count);
+			}
+		}
+	_putchar('\n');
+}
