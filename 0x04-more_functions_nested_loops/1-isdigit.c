a85a62152222b85c4068a48293d3d6a633f0b593
root <Ahmadj.file@gmail.com>
2022-02-01 19:48:43 -0800

    isdigit/q2 try2

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _isdigit - return digit c as 1 
+ * _isdigit - return digit c as 1
  * @c: C is 1
  * Return: (0)
- */
+*/
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
