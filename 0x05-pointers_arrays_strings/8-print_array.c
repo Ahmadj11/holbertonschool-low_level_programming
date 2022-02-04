34596a6ea23b7f86aa3bb005e84b7fa76c2bca53
root <Ahmadj.file@gmail.com>
2022-02-04 08:37:51 -0800

    q8 puts

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,20 +1,22 @@
 #include "main.h"
 /**
- * print_array - print the array 
- * @a: var one 
- * @n: var two 
+ * print_array - print the array
+ * @a: var one
+ * @n: var two
  * Return: 0
  */
 void print_array(int *a, int n)
 {
-	int x;	
-	for(x = 0; x < n; x++)
+	int x;
+
+
+	for (x = 0; x < n; x++)
 	{
 		printf("%d", a[x]);
-		if (x != x - 1)
+		if (x < n - 1)
 		{
 			printf(", ");
 		}
 	}
-	printf("\n")
+	printf("\n");
 }
