0c0bdc8a99f3fa0dd9f222cba8480a2977da05ef
root <Ahmadj.file@gmail.com>
2022-02-07 15:03:07 -0800

    save state2 2/7

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/*
+ * _strncat - main string fiunction 
+ * @dest: destination
+ * @src: source
+ * Return: dest 
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int x;
+	int length;
+
+for ( x = 0;:
