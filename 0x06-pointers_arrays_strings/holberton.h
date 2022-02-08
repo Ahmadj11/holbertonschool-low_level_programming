b986e3de5d06fac9b534b22c345461afb79f171a
root <Ahmadj.file@gmail.com>
2022-02-07 17:29:02 -0800

    q0 try2

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,7 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 #include <unistd.h>
 #include <stdio.h>
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+int _putchar(char c);
 #endif
