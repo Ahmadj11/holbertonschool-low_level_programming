776d033ff6c5128660bb0ae23f5a3f4217b4570f
root <Ahmadj.file@gmail.com>
2022-02-09 11:52:51 -0800

    q0 fix1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,15 @@
-#inlcude "main.h"
+#include "main.h"
 /**
  * _puts_recursion - main function
  * @s: var 1
- * Return: 0
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
-	int x;
-	int y;
-
-	for (x = 0; s[x]
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	_putchar(*s);
+	_put_recursion(s++);
+}
