c8b1cbdb827ba13aa47def8ba16b9e1f5423f274
root <Ahmadj.file@gmail.com>
2022-02-09 14:47:40 -0800

    q1 try2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,23 +1,15 @@
 #include "main.h"
 /**
- * _print_rev - main function
+ *  _print_rev_recursion - main function
  * @s: var 1
  * Return: 0
  */
 void _print_rev_recursion(char *s)
 {
-	int x;
-	int y;
-	char rev;
-
-	for (x = 0; s[x] != '\0'; x++)
+	if (*s == '\0')
 	{
-		continue;
+		return;
 	}
-	for (y = x - 1; y >= x / 2; --y)
-	{
-		rev = s[y];
-		s[y] = s[x - y - 1];
-		s[x - y - 1] = rev;
-	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
