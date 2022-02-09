a3161662e77e0940100cf0c3f4413d0cfc1b4b5d
root <Ahmadj.file@gmail.com>
2022-02-09 15:43:09 -0800

    q3 try1

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * factorial - prints factorial
+ * @n: var
+ * Return: returns factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
