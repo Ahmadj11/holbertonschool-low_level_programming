1186e3fe4d3fe3911a0cf5d339923253c54dfffb
root <Ahmadj.file@gmail.com>
2022-02-09 18:48:14 -0800

    save state3 2/9

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,16 +6,3 @@
  */
 int _sqrt_recursion(int n)
 {
-	int x;
-
-	if (x * x == n)
-	{
-		return (x);
-	}
-	else if (x * x > n)
-	{
-		return (-1);
-	}
-	else
-		return (_sqrt_recursion(x + 1, n));
-}
