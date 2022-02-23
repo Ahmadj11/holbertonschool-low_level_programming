43e68d205639234e17da9d68ad91900111626e3a
root <Ahmadj.file@gmail.com>
2022-02-22 18:01:09 -0800

    newdog2

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,10 +15,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog = malloc(sizeof(struct dog));
 	if (new_dog != NULL)
 	{
-		new_dog->name = name;
-		new_dog->age = age;
-		new_dog->owner = owner;
-		return (new_dog);
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+	return (new_dog);
 	}
 	else
 		return (NULL);
