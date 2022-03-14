0163354b33be7b81c3bacb31b7faed727843dee9
root <Ahmadj.file@gmail.com>
2022-03-14 13:50:19 -0700

    plus ultra1

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_bit - set value of bit
+ * @n: points to number
+ * @index: positions of values
+ * Return: 1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(*n) * 8))
+	{
+		return (-1);
+	}
+	*n |= 1 << index;
+	return (1);
+}
