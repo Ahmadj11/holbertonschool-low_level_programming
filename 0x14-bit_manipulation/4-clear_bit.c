a458e555c6d179da3151fe2a9ba28c1208409602
root <Ahmadj.file@gmail.com>
2022-03-14 14:05:38 -0700

    clear2

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,10 +8,14 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > sizeof(unsigned long int) * 8)
-	{
-		return (-1);
-	}
-	*n &= ~(1 << index);
-	return (0);
+	unsigned long int flag = 1;
+
+	if (n == NULL)
+	return (-1);
+	if (index >= sizeof(unsigned long int) * 8)
+	return (-1);
+	flag = flag << index;
+	flag = ~flag;
+	*n = *n & flag;
+	return (1);
 }
